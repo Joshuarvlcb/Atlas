1e5f093f6b4ed7dd45883285701d78ac502c78f2
josh <joshuarvlcb@gmail.com>
2021-05-11 08:34:28 -0700

    made the page look nice on large screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,70 @@
-import React from 'react'
-import {Nav} from 'react-bootstrap'
-import NavLinks from './NavLinks'
-import Header from './Header'
-import DailyCard from './DailyCard'
+import React from "react";
+import { Nav } from "react-bootstrap";
+import NavLinks from "./NavLinks";
+import Header from "./Header";
+import DailyCard from "./DailyCard";
 import { BiHome } from "react-icons/bi";
-import {Ri24HoursLine} from 'react-icons/ri'
-import {BiCalendarWeek} from 'react-icons/bi'
-import {FiSettings} from 'react-icons/fi'
-import Logo from '../assests/weather-logo.png'
+import { Ri24HoursLine } from "react-icons/ri";
+import { BiCalendarWeek } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
+import Logo from "../assests/weather-logo.png";
 
+const Navbar = ({ currentData }) => {
+  return (
+    <div className="nav-container" style={{ margin: "0" }}>
+      <Header logo={Logo} />
 
-const Navbar = ({currentData}) => {
-    return <div className = 'nav-container' style = {{margin:'0'}}>
-    <Header logo = {Logo}/>
+      <Nav
+        className="d-flex flex-column justify-content-between"
+        style={{ paddingLeft: "25px", height: "30%" }}
+      >
+        <NavLinks
+          name="Today"
+          logo={
+            <BiHome
+              color="white"
+              style={{ paddingRight: "10px", height: "33px", width: "33px" }}
+            />
+          }
+        >
+          {" "}
+          <div onClick={currentData}></div>{" "}
+        </NavLinks>
 
-    <Nav  className = 'd-flex flex-column' style = {{paddingLeft:'25px',height:'30%'}}>
-    <NavLinks    name = 'Today' logo = {<BiHome color = 'white' style = {{paddingRight:'10px',height:'33px',width:'33px'}} />}> <div onClick = {currentData}></div> </NavLinks>   
-    
-    <NavLinks onClick = {() => console.log('hi')} name = 'Hourly' logo = {<Ri24HoursLine color = 'white' style = {{paddingRight:'10px',height:'33px',width:'33px'}} />} />
+        <NavLinks
+          onClick={() => console.log("hi")}
+          name="Hourly"
+          logo={
+            <Ri24HoursLine
+              color="white"
+              style={{ paddingRight: "10px", height: "33px", width: "33px" }}
+            />
+          }
+        />
 
-    <NavLinks  name = 'Daily' logo = {<BiCalendarWeek color = 'white' style = {{paddingRight:'10px',height:'33px',width:'33px'}} />}> 
-    
-    </NavLinks> 
+        <NavLinks
+          name="Daily"
+          logo={
+            <BiCalendarWeek
+              color="white"
+              style={{ paddingRight: "10px", height: "33px", width: "33px" }}
+            />
+          }
+        ></NavLinks>
 
-    <NavLinks name = 'Settings' logo = {<FiSettings color = 'white' style = {{paddingRight:'10px',height:'33px',width:'33px'}} />} />
-    </Nav>
-    <DailyCard/>
+        <NavLinks
+          name="Settings"
+          logo={
+            <FiSettings
+              color="white"
+              style={{ paddingRight: "10px", height: "33px", width: "33px" }}
+            />
+          }
+        />
+      </Nav>
+      <DailyCard />
     </div>
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
