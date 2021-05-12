012f3a881d7131a95a0a2bd94e144f9dc4bd8c22
Kevin <kevinator900@gmail.com>
2021-05-12 14:59:25 -0700

    added slider to hourly page

--- src/components/HourlySlider.jsx
+++ src/components/HourlySlider.jsx
@@ -3,11 +3,22 @@ import MiniCard from './MiniCard'
 import Sun from '../assests/sunny.png'
 import Carousel from 'react-bootstrap/Carousel'
 import { useState } from 'react'
+import {ImArrowRight2, ImArrowLeft2} from 'react-icons/im'
 
 
 const HourlySlider = ({ arr, city, active }) => {
 
-    const slides = [arr.slice(0, 6), arr.slice(6, 12), arr.slice(12, 18), arr.slice(18, 24), arr.slice(24, 30), arr.slice(30, 36), arr.slice(36, 42), arr.slice(42)]
+  const [currSlide, setCurrSlide] = useState(0)
+
+  const slides = [arr.slice(0, 6), arr.slice(6, 12), arr.slice(12, 18), arr.slice(18, 24), arr.slice(24, 30), arr.slice(30, 36), arr.slice(36, 42), arr.slice(42)]
+
+  const next = () => {
+    setCurrSlide((currSlide === 7) ? 0 : currSlide + 1)
+  }
+
+  const back = () => {
+    setCurrSlide((currSlide === 0) ? 7 : currSlide - 1)
+  }
 
     return(
     <div className="slider">
@@ -22,16 +33,13 @@ const HourlySlider = ({ arr, city, active }) => {
       {city}
       </h3>
       <div className="daily-slider">
-          <Carousel slide wrap touch interval = '100'>
-              {slides.map( (slide, i) => {
-                return (<Carousel.Item className = "d-flex justify-content-evenly" key = {`s${i}`} style={{backgroundColor: "rgb(243, 243, 243)"}}>
-                    {slide.map( (card, k) => {
-                        return <MiniCard pic={Sun} temp = {card.temp} text={`${i}${k}`} key = {`s${i}c${k}`}/>;
-                    })}
-                </Carousel.Item>)
-              })}
-              
-          </Carousel>
+          {slides[currSlide].map( (card, i) => {
+            return <MiniCard pic={Sun} temp = {card.temp} text={`${(currSlide) * 6 + i + 1}:00`} key = {`weather${i}`} arr =  {arr}/>
+          })}
+      </div>
+      <div className="buttonContainer">
+        <ImArrowLeft2 onClick = {back}/>
+        <ImArrowRight2 onClick = {next}/>
       </div>
     </div>)
 }
