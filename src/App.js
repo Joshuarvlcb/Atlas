08f6e6aebf1748f9d3016d52c634c623c9b7eae7
joshua <joshuarvlcb@gmail.com>
2021-05-14 15:24:32 -0700

    making the chart

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import React, { useState } from "react";
 import Slider from "./components/Slider";
 import Navbar from "./components/Navbar";
 import GuestIcon from "./assests/guest-icon.png";
-import { FaArrowRight } from "react-icons/fa";
 import Chart from "./components/Chart";
-import { BrowserRouter as Router, Switch as S, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch as S, Route,Link,Redirect } from "react-router-dom";
 import weather from './assests/weather.json'
 import Settings from './components/Settings'
 import HourlySlider from "./components/HourlySlider.jsx";
@@ -169,8 +168,6 @@ function App() {
                 style={{
                   paddingBottom: "20px",
                   paddingLeft: "20px",
-                  width: "269px",
-                  alignItems: "center",
                 }}
               >
                 Weather Forecast
@@ -180,21 +177,25 @@ function App() {
                 <Cards activeF={activeCity} data={cityData} />
               </div>
             </div>
-            <div className="details">
-              <h5>
-                Details more <FaArrowRight style={{ marginLeft: "11px" }} />
-              </h5>
-            </div>
+
+          
+
+            <Router>
+         
             
-             <Router>
               <Route path="/chart" component={Chart} />
+            
               <Route
                 path="/slider"
                 component={() => {
                   return (activePage !== "hourly") ? (<Slider miniData = {miniCardData} arr={data[activePage]} city={city} active = {activePage}/>)
                    : (<HourlySlider miniData = {miniCardData} arr = {data['hourly']} city = {city} active = {activePage} currSlide = {currSlide} setCurrSlide = {(val) => {setCurrSlide(val)}}/>);
                 }}
-              />
+                />
+                      
+
+                <Redirect to = '/slider'></Redirect>
+
             </Router> 
              
             </div>) : <Settings/>}
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,18 +1,51 @@
-// import { Doughnut } from 'react-chartjs-2';
+import React from 'react'
+import { Line } from 'react-chartjs-2';
 
-// const Chart = () => {
-//   return (
-//     <div style={{ height: "50%" }}>
-//       <h5>Chart</h5>
-//       <Doughnut
-//         height={100}
-//         width={100}
-//         data={{
-//           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         }}
-//       />
-//     </div>
-//   );
-// };
 
-// export default Chart;
+const Chart = () => {
+
+
+  return (
+      <>
+    <div className = 'chart-con d-flex align-items-center flex-column' style = {{height:'50%'}}>
+  <h3
+                style={{
+                  paddingBottom: "20px",
+                  paddingLeft: "20px",
+                }}
+              >Chart</h3>
+
+<Line style = {{padding:'0 25px'}}
+height = '70%'
+      data={{
+        labels: ['1', '2', '3', '4', '5', '6'],
+        dataSets:[
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      }}
+      options = {{
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      }}
+    />
+
+    </div>
+    
+    </>
+  );
+};
+
+export default Chart;
