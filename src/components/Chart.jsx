2c707d77b658613266621c694e006cca6caf8e36
Kevin <kevinator900@gmail.com>
2021-05-19 15:20:14 -0700

    got chart to display properly

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,8 +1,47 @@
 import React from "react";
-// import { Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2"
 import Detail from "./Detail";
 
-const Chart = ({activePage, chartToggle,chart}) => {
+const Chart = ({activePage, chartToggle,chart, newData}) => {
+
+
+  const labels = {
+    today: ['Morning', 'Noon', 'Evening', 'Night'],
+    daily: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    hourly: ['6:00', '12:00', '18:00', '24:00', '36:00', '42:00', '48:00']
+  }
+  const data = {
+    labels: labels[activePage],
+    datasets: [
+      {
+        data: [10, 20, 15, 45],
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+      },
+    ],
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+      // xAxes: [
+      //   {
+      //     ticks: {
+      //       beginAtZero: true,
+      //     }
+      //   }
+      // ]
+    },
+  };
+
   return (
     <>
       <div
@@ -11,7 +50,7 @@ const Chart = ({activePage, chartToggle,chart}) => {
       >
         <div
           className=" d-flex justify-content-between align-items-center"
-          style={{ width: "100%"  ,             paddingBottom: "20px"
+          style={{ width: "100%"  , paddingBottom: "20px"
         }}
         >
           <h3
@@ -24,34 +63,12 @@ const Chart = ({activePage, chartToggle,chart}) => {
  
           <Detail activePage = {activePage} chartToggle = {chartToggle} chart={chart}/>
         </div>
-
-        {/* <Line
+        <Line
           style={{ padding: "0 25px" }}
           height="70%"
-          data={{
-            labels: ["1", "2", "3", "4", "5", "6"],
-            dataSets: [
-              {
-                label: "# of Votes",
-                data: [12, 19, 3, 5, 2, 3],
-                fill: false,
-                backgroundColor: "rgb(255, 99, 132)",
-                borderColor: "rgba(255, 99, 132, 0.2)",
-              },
-            ],
-          }}
-          options={{
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true,
-                  },
-                },
-              ],
-            },
-          }}
-        /> */}
+          data= {data}
+          options={options}
+        />
       </div>
     </>
   );
